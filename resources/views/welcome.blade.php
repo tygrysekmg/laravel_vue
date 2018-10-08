6bf18f05357da75065aa4fc2e8bb8147d52f5a0f
Grzegorz Sowa <w.luk1972@wp.pl>
2018-10-08 17:19:45 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <div id="app">
+		
+		<div class="container">
+		<articles></articles>
+		</div>
+		</div>
+		<script src="{{ asset('js/app.js') }}" ></script>
+            
+            
+    </body>
+</html>
